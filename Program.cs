e864121b9700ddf04c7922339fea19b8123ae720
prince475 <danielmwai475@gmail.com>
2023-05-24 00:03:48 +0300

    Creating a FibonacciSequence for the first 20 elements.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> {1,1};
+
+            while(fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+            {
+            Console.WriteLine(item);
+            }
+
+            // challenge get the first 20 fibonacci numbers.
+
+
+
+        }
+    }
+}
+
+
